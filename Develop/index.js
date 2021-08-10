1d49585b71ac2a5c04e10e6ed9f02cd8d413f940
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-08-10 15:26:55 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the name of your project? (Required)'
+    },
+    {
+        type: 'input',
+        name: 'projectInformation',
+        message: 'What are the sections of the project?',
+    },
+    {
+        type: 'input',
+        name: 'projectInstallation',
+        message: 'How will the installation of the project go?',
+    },
+    {
+        type: 'input',
+        name: 'projectUsage',
+        message: "What will be the project be useful for?",
+    },
+    {
+        type: 'input',
+        name: 'projectContributing',
+        message: 'What will be contributing to this project?',
+    },
+    {
+        type: 'input',
+        name: 'projectTest',
+        message: 'How will the project be tested?',
+    },
+    {
+        type: 'input',
+        name: 'projectQuestions',
+        message: 'What questions will this project answer?',
+    }
+    
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
